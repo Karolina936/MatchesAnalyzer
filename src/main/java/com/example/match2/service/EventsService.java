7c8cbeedd16e2bc851c8860a0e8aeca0d87842c8
Karolina936 <75604545+Karolina936@users.noreply.github.com>
2023-01-19 19:10:34 +0100

    Data structure to store unique team name

--- src/main/java/com/example/match2/service/EventsService.java
+++ src/main/java/com/example/match2/service/EventsService.java
@@ -6,22 +6,37 @@ import com.example.match2.model.Events;
 import org.springframework.stereotype.Service;
 
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 @Service
 public class EventsService {
 
     public EventsService(){};
     MapJasonToObject mapper = new MapJasonToObject();
-    Events events = mapper.mapping();
+    List<Event> events = mapper.mapping().getEvents();
+
+
+
     public List<Event> get10Results() {
 
-        return events.getEvents().stream().limit(10).toList();
+        return events.stream().limit(10).toList();
     }
 
     public List<Event> getResults(int number) {
 
-        int numberOfEvents = events.getEvents().size();
-        return mapper.mapping().getEvents().stream().limit((number > numberOfEvents)?  numberOfEvents : number).toList();
+        int numberOfEvents = events.size();
+        return events.stream().limit((number > numberOfEvents)?  numberOfEvents : number).toList();
+    }
+
+    public List<String> getTeamsNamesAlphabetically(){
+
+        Set<String> treeSet = new TreeSet<>();
+        treeSet.addAll(events.stream().map(events -> events.getCompetitors().get(0).getName()).toList());
+        treeSet.addAll(events.stream().map(events -> events.getCompetitors().get(1).getName()).toList());
+
+       return treeSet.stream().toList();
     }
 }
